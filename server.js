8cf20e17a588a358ad2fc1a70210373165305cb3
ranga009-bit <ranga.009@gmail.com>
2026-04-28 10:59:52 +0530

    Add CORS support to Express server

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 const app = express();
 
 // MIDDLEWARE
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 // ================= DB CONNECTION =================
