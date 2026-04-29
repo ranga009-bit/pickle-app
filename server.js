6ef94a00ac3f5495e0639dd80f14157bbc75f2da
ranga009-bit <ranga.009@gmail.com>
2026-04-29 20:07:25 +0530

    Implement user authentication with signup and login
    Added user signup and login routes with JWT authentication.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const User = require("./models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const app = express();
 
 // MIDDLEWARE
@@ -90,7 +93,72 @@ app.delete("/products/:id", async (req, res) => {
   }
 });
 // ================= SERVER =================
+// SIGNUP
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+
+    res.json({
+      success: true,
+      message: "Signup successful"
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: "Signup failed" });
+  }
+});
+
+
+// LOGIN
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(400).json({ message: "Wrong password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      "pickleSecretKey",
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      success: true,
+      message: "Login successful",
+      token,
+      name: user.name
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: "Login failed" });
+  }
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
